8aa4ad84a355510f527271ee7c660d1e4ee0cfa2
azxki <cococoursan@gmail.com>
2022-12-16 13:36:13 +0100

    meteo

--- app.js
+++ app.js
@@ -1,23 +1,61 @@
+let ville = document.getElementById('ville');
+let date = document.getElementById('date');
+let degres = document.getElementById('degres');
+let button = document.getElementById('button');
+let input = document.getElementById('input');
+
+let aujourdhui = new Date();
+date.innerHTML = aujourdhui.toLocaleDateString();
 
 
-const requestURL =
-    "https://api.openweathermap.org/data/2.5/weather?lat=50.0153&lon=4.0478&appid=8a4a001f1348b4bd88e32c9272eb8994";
+let printPosition = function (coordinates) {
+    console.log(coordinates)
 
-let xhr = new XMLHttpRequest();
+    let requestURL =
+        "https://api.openweathermap.org/data/2.5/weather?lat=" + `${coordinates.coords.latitude}` + "&lon=" + `${coordinates.coords.longitude}` + "&appid=8a4a001f1348b4bd88e32c9272eb8994&units=metric";
 
-xhr.open("GET", requestURL);
-xhr.responseType = "json";
+    button.addEventListener('click', function () {
+        requestURL =
+            "https://api.openweathermap.org/data/2.5/weather?q=" + input.value + "&appid=8a4a001f1348b4bd88e32c9272eb8994&units=metric";
 
-xhr.onload = function () {
-    if (xhr.status !== 200) {
-        alert("Une erreur est survenue !");
-        return;
+        let xhr = new XMLHttpRequest();
+
+        xhr.open("GET", requestURL);
+        xhr.responseType = "json";
+
+        xhr.onload = function () {
+            if (xhr.status !== 200) {
+                alert("Une erreur est survenue !");
+                return;
+            }
+
+            let response = xhr.response;
+            console.log(response);
+            ville.innerHTML = response.name + ", " + response.sys.country;
+            degres.innerHTML = Math.trunc(response.main.temp) + "°C";
+        }
+
+        xhr.send();
+    })
+    xhr = new XMLHttpRequest();
+
+    xhr.open("GET", requestURL);
+    xhr.responseType = "json";
+
+    xhr.onload = function () {
+        if (xhr.status !== 200) {
+            alert("Une erreur est survenue !");
+            return;
+        }
+
+        let response = xhr.response;
+        console.log(response);
+        ville.innerHTML = response.name + ", " + response.sys.country;
+        degres.innerHTML = Math.trunc(response.main.temp) + "°C";
     }
 
-    let response = xhr.response;
-    console.log(response);
-    let wind = response.wind;
-    alert("Hello " + wind.deg + " " + wind.gust);
+    xhr.send();
 }
 
-xhr.send();
+
+navigator.geolocation.getCurrentPosition(printPosition);
