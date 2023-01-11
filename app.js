3014853ec836f7f2f22c673ec2ec83758bb768f9
azxki <cococoursan@gmail.com>
2023-01-11 15:06:09 +0100

    meteo

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ let degres = document.getElementById('degres');
 let button = document.getElementById('button');
 let input = document.getElementById('input');
 let humidity = document.getElementById('humidite');
+let sunrise = document.getElementById('sunrise');
+let sunset = document.getElementById('sunset');
+let temps = document.getElementById('temps');
+let visibility = document.getElementById('visibility');
 
 let aujourdhui = new Date();
 date.innerHTML = aujourdhui.toLocaleDateString();
@@ -56,13 +60,20 @@ let printPosition = function (coordinates) {
         let response = xhr.response;
         console.log(response);
         ville.innerHTML = response.name + ", " + response.sys.country;
-        degres.innerHTML = "Température " + Math.trunc(response.main.temp) + "°C <br> Température max : " +
-            Math.trunc(response.main.temp_max) + "°C <br> Température min : " + Math.trunc(response.main.temp_min) + "°C";
+        degres.innerHTML = "Température " + Math.trunc(response.main.temp) + "° <br> Température max : " +
+            Math.trunc(response.main.temp_max) + "° <br> Température min : " + Math.trunc(response.main.temp_min) + "°";
+        visibility.innerHTML = "visibilité de " + response.visibility/1000 + "km";
+        sunrise.innerHTML = new Date (response.sys.sunrise*1000).getHours() + "h" + new Date (response.sys.sunrise*1000).getMinutes();
+        sunset.innerHTML = new Date (response.sys.sunset*1000).getHours() + "h" + new Date (response.sys.sunset*1000).getMinutes();
+        temps.innerHTML = response.weather[2];
         degres.style.fontSize = "130%";
         humidity.innerHTML = "Humidité : " + Math.trunc(response.main.humidity) + "%";
         humidity.style.fontSize = "110%";
-    }
 
+        if (response.weather.main === "Clouds") {
+            document.body.style.backgroundImage = "url('/nuageux.gif')";
+        }
+    }
     xhr.send();
 }
 
